fb9d18c7876fbf0aba82261ae0c3bd7378829ae0
howardsolutions <phungthinh0946519356@gmail.com>
2025-02-16 17:51:16 -0700

    test: test cases for character

--- examples/characters/src/character.test.ts
+++ examples/characters/src/character.test.ts
@@ -5,10 +5,34 @@ import { Person } from './person.js';
 describe('Character', () => {
   it(
     'should create a character with a first name, last name, and role',
-    () => {},
+    () => {
+      const character = new Character('Howard', "Phung", "Senior Software ML Engineer");
+      // expect(character).toMatchObject(character);
+      expect(character).toEqual(expect.objectContaining({
+        firstName: "Howard",
+        lastName: "Phung",
+        role: "Senior Software ML Engineer"
+      }))
+    },
   );
 
-  it.todo('should allow you to increase the level', () => {});
+  it('should allow you to increase the level', () => {
+    const character = new Character('Howard', "Phung", "Senior Software ML Engineer");
+    const initialLevel = character.level;
 
-  it.todo('should update the last modified date when leveling up', () => {});
+    character.levelUp();
+    expect(character.level).toBeGreaterThan(initialLevel);
+  });
+
+  it('should update the last modified date when leveling up', () => {
+    const character = new Character('Howard', "Phung", "Senior Software ML Engineer");
+    const currentLastModified = character.lastModified;
+    const initialLevel = character.level;
+
+
+    character.levelUp();
+    expect(character.level).toBeGreaterThan(initialLevel);
+
+    expect(character).not.toBe(currentLastModified)
+  });
 });
