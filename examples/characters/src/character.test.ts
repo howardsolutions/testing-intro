45fbcf0fc36ac9dba210d7ae37470b8cfc8a82b1
howardsolutions <phungthinh0946519356@gmail.com>
2025-02-16 17:58:24 -0700

    chore: add beforeEach hook

--- examples/characters/src/character.test.ts
+++ examples/characters/src/character.test.ts
@@ -3,10 +3,15 @@ import { Character } from './character.js';
 import { Person } from './person.js';
 
 describe('Character', () => {
+  let character;
+
+  beforeEach(() => {
+    character = new Character('Howard', "Phung", "Senior Software ML Engineer");
+  });
+
   it(
     'should create a character with a first name, last name, and role',
     () => {
-      const character = new Character('Howard', "Phung", "Senior Software ML Engineer");
       // expect(character).toMatchObject(character);
       expect(character).toEqual(expect.objectContaining({
         firstName: "Howard",
@@ -17,7 +22,6 @@ describe('Character', () => {
   );
 
   it('should allow you to increase the level', () => {
-    const character = new Character('Howard', "Phung", "Senior Software ML Engineer");
     const initialLevel = character.level;
 
     character.levelUp();
@@ -25,12 +29,12 @@ describe('Character', () => {
   });
 
   it('should update the last modified date when leveling up', () => {
-    const character = new Character('Howard', "Phung", "Senior Software ML Engineer");
     const currentLastModified = character.lastModified;
     const initialLevel = character.level;
 
 
     character.levelUp();
+
     expect(character.level).toBeGreaterThan(initialLevel);
 
     expect(character).not.toBe(currentLastModified)
