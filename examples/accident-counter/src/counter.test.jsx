1e9e70571b3ecd7eca20a6ad259089bc4225d83b
howardsolutions <phungthinh0946519356@gmail.com>
2025-02-24 10:50:01 -0700

    feat: add initialCount prop to Counter component and update tests

--- examples/accident-counter/src/counter.test.jsx
+++ examples/accident-counter/src/counter.test.jsx
@@ -1,4 +1,4 @@
-import { render, screen } from '@testing-library/react';
+import { act, render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 
 import { Counter } from './counter';
@@ -6,16 +6,16 @@ import { Counter } from './counter';
 import '@testing-library/jest-dom/vitest';
 
 describe('Counter ', () => {
-  beforeEach(() => {
-    render(<Counter />);
-  });
-
   it('renders with an initial count of 0', () => {
+    render(<Counter />);
+
     const counter = screen.getByTestId('counter-count');
     expect(counter).toHaveTextContent('0');
   });
 
   it('disables the "Decrement" and "Reset" buttons when the count is 0', () => {
+    render(<Counter />);
+
     const decrementBtn = screen.getByRole('button', { name: /decrement/i });
     const resetBtn = screen.getByRole('button', { name: /reset/i });
 
@@ -24,11 +24,15 @@ describe('Counter ', () => {
   });
 
   it('displays "days" when the count is 0', () => {
+    render(<Counter />);
+
     const unit = screen.getByTestId('counter-unit');
     expect(unit).toHaveTextContent('days');
   });
 
   it('increments the count when the "Increment" button is clicked', async () => {
+    render(<Counter />);
+
     const incrementBtn = screen.getByRole('button', { name: /increment/i });
     const counter = screen.getByTestId('counter-count');
     await userEvent.click(incrementBtn);
@@ -36,6 +40,8 @@ describe('Counter ', () => {
   });
 
   it('displays "day" when the count is 1', async () => {
+    render(<Counter />);
+
     const incrementBtn = screen.getByRole('button', { name: /increment/i });
     await userEvent.click(incrementBtn);
     const unit = screen.getByTestId('counter-unit');
@@ -43,13 +49,22 @@ describe('Counter ', () => {
   });
 
   it('decrements the count when the "Decrement" button is clicked', async () => {
+    render(<Counter initialCount={1} />);
+
     const decrementBtn = screen.getByRole('button', { name: /decrement/i });
+    expect(decrementBtn).not.toBeDisabled();
     const counter = screen.getByTestId('counter-count');
+    // await act(async () => {
+    //   await userEvent.click(decrementBtn);
+    // });
     await userEvent.click(decrementBtn);
     expect(counter).toHaveTextContent('0');
+    expect(decrementBtn).toBeDisabled();
   });
 
   it('does not allow decrementing below 0', async () => {
+    render(<Counter />);
+
     const decrementBtn = screen.getByRole('button', { name: /decrement/i });
     const counter = screen.getByTestId('counter-count');
     await userEvent.click(decrementBtn);
@@ -57,6 +72,8 @@ describe('Counter ', () => {
   });
 
   it('resets the count when the "Reset" button is clicked', async () => {
+    render(<Counter />);
+
     const resetBtn = screen.getByRole('button', { name: /reset/i });
     const incrementBtn = screen.getByRole('button', { name: /increment/i });
     const counter = screen.getByTestId('counter-count');
@@ -67,6 +84,8 @@ describe('Counter ', () => {
   });
 
   it('disables the "Decrement" and "Reset" buttons when the count is 0', () => {
+    render(<Counter />);
+
     const decrementBtn = screen.getByRole('button', { name: /decrement/i });
     const resetBtn = screen.getByRole('button', { name: /reset/i });
 
@@ -75,6 +94,8 @@ describe('Counter ', () => {
   });
 
   it('updates the document title based on the count', async () => {
+    render(<Counter />);
+
     const incrementBtn = screen.getByRole('button', { name: /increment/i });
     await userEvent.click(incrementBtn);
     await userEvent.click(incrementBtn);
