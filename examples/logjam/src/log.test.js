8644477009d558f3407a579f76c43465176dd2a0
howardsolutions <phungthinh0946519356@gmail.com>
2025-03-05 13:16:11 -0700

    test: add comprehensive test cases for logger in development and production modes

--- examples/logjam/src/log.test.js
+++ examples/logjam/src/log.test.js
@@ -1,4 +1,41 @@
 import { expect, it, vi, beforeEach, afterEach, describe } from 'vitest';
 import { log } from './log';
 
-describe.todo('logger', () => {});
+describe('logger', () => {
+  describe('development', () => {
+    beforeEach(() => {
+      vi.stubEnv('MODE', 'development');
+    });
+
+    afterEach(() => {
+      vi.resetAllMocks();
+    });
+
+    it('logs to the console in development mode', () => {
+      const logSpy = vi.spyOn(console, 'log');
+
+      log('hello');
+
+      expect(logSpy).toHaveBeenCalled();
+      expect(logSpy).toHaveBeenCalledWith('hello');
+    });
+  });
+  
+  describe('production', () => {
+    beforeEach(() => {
+      vi.stubEnv('MODE', 'production');
+    });
+
+    afterEach(() => {
+      vi.resetAllMocks();
+    });
+
+    it('send to the server the message', () => {
+      const logSpy = vi.spyOn(console, 'log');
+
+      log('hello');
+
+      expect(logSpy).not.toHaveBeenCalled();
+    });
+  });
+});
